98026d74b3a41e728d4aee695b76d1fc48df6d1b
slaviktk <slavikt9@gmail.com>
2018-06-09 11:19:11 +0300

    ok

--- src/lesson8.java
+++ src/lesson8.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class lesson8 {
     public static void main (String [] args) {
         try (Scanner sc = new Scanner (System.in)){
-            /*System.out.println("Imput number a:");
+            /*System.out.print("Imput number a:");
             int a = sc.nextInt();
-            System.out.println("Imput number b:");
+            System.out.print("Imput number b:");
             int b = sc.nextInt();
-            System.out.println("Imput number c:");
+            System.out.print("Imput number c:");
             int c = sc.nextInt();
             System.out.println("c="+c);
             System.out.println("b="+b);
             System.out.println("a="+a);
             System.out.println("Programm done");
 */
-
+            int a = sc.nextInt();
+            while (a>0) {
+                System.out.println("Ya Programyst");
+                a--;
+            }
         }
 
     }
