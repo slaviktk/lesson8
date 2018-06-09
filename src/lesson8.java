74de8dd66a57fb0c839fd7787c68b045754be102
hillel <hillel@iMac-hillel-2.WORKGROUP>
2018-06-09 10:37:07 +0300

    Initial commit

---
+++ src/lesson8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lesson8 {
+    public static void main (String [] args) {
+        try (Scanner sc = new Scanner (System.in)){
+            System.out.println("Imput number a:");
+            int a = sc.nextInt();
+            System.out.println("Imput number b:");
+            int b = sc.nextInt();
+            System.out.println("Imput number c:");
+            int c = sc.nextInt();
+            System.out.println("c="+c);
+            System.out.println("b="+b);
+            System.out.println("a="+a);
+
+        }
+
+    }
+}
